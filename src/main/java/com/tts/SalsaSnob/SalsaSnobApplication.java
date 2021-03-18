b1ad7be801c82a4e893417a0efeb00c4e7599573
Mary Sutherland <mary.sutherland.atx@gmail.com>
2021-03-18 09:26:01 -0500

    first commit

---
+++ src/main/java/com/tts/SalsaSnob/SalsaSnobApplication.java
@@ -0,0 +1,13 @@
+package com.tts.SalsaSnob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsaSnobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsaSnobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/SalsaSnob/SalsaSnobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.SalsaSnob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsaSnobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
